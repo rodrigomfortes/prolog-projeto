d2ec8939c7853b4a3ca199f8b646350948b1d65f
Nicolas Kleiton <inicolaskleiton@gmail.com>
2025-04-29 21:18:38 -0300

    feat: adicionando mais series e animes em dados.pl

--- dados.pl
+++ dados.pl
@@ -6,7 +6,7 @@
 
 % Ação
 
-% Filmes
+% Filmes de Ação
 filme(matrix, acao).
 filme(o_cavaleiro_das_trevas, acao).
 filme(gladiador, acao).
@@ -18,7 +18,7 @@ filme(skyfall, acao).
 filme(john_wick, acao).
 filme(o_cavaleiro_das_trevas_ressurge, acao).
 
-% Notas dos filmes
+% Notas dos filmes de Ação
 nota(matrix, 8.7).
 nota(o_cavaleiro_das_trevas, 9.0).
 nota(gladiador, 8.5).
@@ -30,37 +30,57 @@ nota(skyfall, 8.0).
 nota(john_wick, 7.8).
 nota(o_cavaleiro_das_trevas_ressurge, 8.4).
 
-% Séries
+% Séries de Ação
 serie(la_casa_de_papel, acao).
 serie(daredevil, acao).
 serie(flash, acao).
 serie(arrow, acao).
 serie(supergirl, acao).
+serie(the_punisher, acao).
+serie(jessica_jones, acao).
+serie(agents_of_shield, acao).
+serie(umbrella_academy, acao).
+serie(the_walking_dead, acao).
 
-% Notas das séries
+% Notas das séries de Ação
 nota(la_casa_de_papel, 8.3).
 nota(daredevil, 8.6).
 nota(flash, 7.9).
 nota(arrow, 7.8).
 nota(supergirl, 7.2).
+nota(the_punisher, 8.5).
+nota(jessica_jones, 8.0).
+nota(agents_of_shield, 7.5).
+nota(umbrella_academy, 8.0).
+nota(the_walking_dead, 8.3).
 
-% Animes
+% Animes de Ação
 anime(naruto, acao).
 anime(one_piece, acao).
 anime(attacks_on_titan, acao).
 anime(bleach, acao).
 anime(boku_no_hero, acao).
+anime(dragons, acao).
+anime(jojos_bizarre_adventure, acao).
+anime(black_clover, acao).
+anime(demon_slayer, acao).
+anime(sword_art_online, acao).
 
-% Notas dos animes
+% Notas dos animes de Ação
 nota(naruto, 8.5).
 nota(one_piece, 8.7).
 nota(attacks_on_titan, 8.9).
 nota(bleach, 8.3).
 nota(boku_no_hero, 8.7).
+nota(dragons, 7.2).
+nota(jojos_bizarre_adventure, 8.6).
+nota(black_clover, 8.4).
+nota(demon_slayer, 8.8).
+nota(sword_art_online, 8.1).
 
 % Ficção
 
-% Filmes
+% Filmes de Ficção
 filme(interestelar, ficcao).
 filme(o_mestre_do_spirit, ficcao).
 filme(duna, ficcao).
@@ -86,179 +106,255 @@ nota(her, 8.0).
 nota(origem, 8.8).
 nota(a_maquina_do_tempo, 7.5).
 
-% Séries de ficção
+% Séries de Ficção
 serie(stranger_things, ficcao).
 serie(black_mirror, ficcao).
 serie(westworld, ficcao).
 serie(the_expanse, ficcao).
 serie(alienist, ficcao).
+serie(dark, ficcao).
+serie(lost, ficcao).
+serie(stargate_sg1, ficcao).
+serie(black_lagoon, ficcao).
 
-% Notas das séries de ficção
+% Notas das séries de Ficção
 nota(stranger_things, 8.8).
 nota(black_mirror, 8.7).
 nota(westworld, 8.7).
 nota(the_expanse, 8.5).
 nota(alienist, 7.9).
+nota(dark, 8.8).
+nota(lost, 8.3).
+nota(stargate_sg1, 8.4).
+nota(black_lagoon, 8.3).
 
-% Animes de ficção
+% Animes de Ficção
 anime(steins_gate, ficcao).
 anime(psycho_pass, ficcao).
 anime(ghost_in_the_shell, ficcao).
 anime(cowboy_bebop, ficcao).
 anime(ergon, ficcao).
+anime(parasyte, ficcao).
+anime(blue_exorcist, ficcao).
+anime(ghost_hunt, ficcao).
+anime(legend_of_the_galactic_heroes, ficcao).
+anime(technolize, ficcao).
 
-% Notas dos animes de ficção
+% Notas dos animes de Ficção
 nota(steins_gate, 8.8).
 nota(psycho_pass, 8.6).
 nota(ghost_in_the_shell, 8.3).
 nota(cowboy_bebop, 8.7).
 nota(ergon, 8.4).
+nota(parasyte, 8.1).
+nota(blue_exorcist, 8.2).
+nota(ghost_hunt, 7.9).
+nota(legend_of_the_galactic_heroes, 9.0).
+nota(technolize, 8.1).
 
 % Aventura
 
-% Filmes de aventura
-filme(parque_jurassico, aventura).
+% Filmes de Aventura
+filme(jurassic_park, aventura).
 filme(o_senhor_dos_aneis_o_retorno_do_rei, aventura).
-filme(harry_potter_e_a_pedra_filosofal, aventura).
+filme(guardioes_da_galaxia, aventura).
 filme(o_hobbit_uma_viagem_inesperada, aventura).
 filme(piratas_do_caribe, aventura).
-filme(quest_for_fire, aventura).
-filme(o_senhor_dos_aneis_as_duas_torres, aventura).
+filme(a_guerra_do_fogo, aventura).
+filme(king_kong, aventura).
 filme(narnia, aventura).
-filme(guardiões_da_galaxia, aventura).
+filme(mad_max_estrada_da_furia, aventura).
 filme(os_vingadores, aventura).
 
-% Notas dos filmes de aventura
-nota(parque_jurassico, 8.1).
+% Notas dos filmes de Aventura
+nota(jurassic_park, 8.1).
 nota(o_senhor_dos_aneis_o_retorno_do_rei, 9.0).
-nota(harry_potter_e_a_pedra_filosofal, 8.3).
+nota(guardioes_da_galaxia, 8.0).
 nota(o_hobbit_uma_viagem_inesperada, 7.8).
 nota(piratas_do_caribe, 8.0).
-nota(quest_for_fire, 7.0).
-nota(o_senhor_dos_aneis_as_duas_torres, 8.7).
+nota(a_guerra_do_fogo, 7.0).
+nota(king_kong, 7.8).
 nota(narnia, 7.5).
-nota(guardiões_da_galaxia, 8.0).
-nota(os_vingadores, 8.0).
 
-% Séries de aventura
+% Séries de Aventura
 serie(jogo_dos_tronos, aventura).
-serie(amigos, aventura).
+serie(friends, aventura).
 serie(o_hobbit, aventura).
 serie(stranger_things, aventura).
 serie(the_witcher, aventura).
+serie(legend_of_korra, aventura).
+serie(terra_nova, aventura).
+serie(narcos, aventura).
 
-% Notas das séries de aventura
+% Notas das séries de Aventura
 nota(jogo_dos_tronos, 9.3).
-nota(amigos, 8.9).
+nota(friends, 8.9).
 nota(o_hobbit, 8.1).
 nota(stranger_things, 8.8).
 nota(the_witcher, 8.0).
+nota(legend_of_korra, 8.3).
+nota(terra_nova, 7.9).
+nota(narcos, 8.8).
 
-% Animes de aventura
+% Animes de Aventura
 anime(spirit_digger, aventura).
 anime(attack_on_titan, aventura).
 anime(one_piece, aventura).
 anime(demon_slayer, aventura).
 anime(fairy_tail, aventura).
+anime(bleach, aventura).
+anime(tower_of_god, aventura).
+anime(fullmetal_alchemist_brotherhood, aventura).
+anime(black_clover, aventura).
+anime(the_rising_of_the_shield_hero, aventura).
 
-% Notas dos animes de aventura
+% Notas dos animes de Aventura
 nota(spirit_digger, 7.8).
 nota(attack_on_titan, 8.9).
 nota(one_piece, 8.7).
 nota(demon_slayer, 8.8).
 nota(fairy_tail, 8.1).
+nota(bleach, 8.3).
+nota(tower_of_god, 8.2).
+nota(fullmetal_alchemist_brotherhood, 9.1).
+nota(black_clover, 8.4).
+nota(the_rising_of_the_shield_hero, 8.0).
 
 % Fantasia
 
-% Filmes de fantasia
-filme(o_senhor_dos_aneis_a_companhia_do_anel, fantasia).
+% Filmes de Fantasia
+filme(o_senhor_dos_aneis, fantasia).
 filme(harry_potter, fantasia).
 filme(a_vida_e_bela, fantasia).
-filme(o_senhor_dos_aneis_as_duas_torres, fantasia).
-filme(o_senhor_dos_aneis_o_retorno_do_rei, fantasia).
+filme(o_mestre_da_magia, fantasia).
+filme(oculos_magicos, fantasia).
+filme(a_caverna_das_luzes, fantasia).
+filme(a_jornada_dos_elementos, fantasia).
+filme(os_magos_da_noite, fantasia).
 
-% Notas dos filmes de fantasia
-nota(o_senhor_dos_aneis_a_companhia_do_anel, 8.8).
+% Notas dos filmes de Fantasia
+nota(o_senhor_dos_aneis, 8.8).
 nota(harry_potter, 8.3).
 nota(a_vida_e_bela, 8.6).
-nota(o_senhor_dos_aneis_as_duas_torres, 8.7).
-nota(o_senhor_dos_aneis_o_retorno_do_rei, 9.0).
+nota(o_mestre_da_magia, 8.4).
+nota(oculos_magicos, 7.9).
+nota(a_caverna_das_luzes, 8.0).
+nota(a_jornada_dos_elementos, 7.8).
+nota(os_magos_da_noite, 7.7).
 
-% Séries de fantasia
+% Séries de Fantasia
 serie(o_witcher, fantasia).
 serie(jogo_dos_tronos, fantasia).
 serie(stranger_things, fantasia).
 serie(the_shannara_chronicles, fantasia).
 serie(a_mandaloriana, fantasia).
+serie(charmed, fantasia).
+serie(the_boys, fantasia).
+serie(marvels_daredevil, fantasia).
+serie(underworld, fantasia).
+serie(american_horror_story, fantasia).
 
-% Notas das séries de fantasia
+% Notas das séries de Fantasia
 nota(o_witcher, 8.0).
 nota(jogo_dos_tronos, 9.3).
 nota(stranger_things, 8.8).
 nota(the_shannara_chronicles, 7.6).
 nota(a_mandaloriana, 8.6).
+nota(charmed, 7.8).
+nota(the_boys, 8.7).
+nota(marvels_daredevil, 8.6).
+nota(underworld, 7.6).
+nota(american_horror_story, 8.3).
 
-% Animes de fantasia
+% Animes de Fantasia
 anime(jojos_bizarre_adventure, fantasia).
 anime(demon_slayer, fantasia).
 anime(fairy_tail, fantasia).
 anime(bleach, fantasia).
 anime(fullmetal_alchemist, fantasia).
+anime(fate_zero, fantasia).
+anime(attack_on_titan, fantasia).
+anime(naruto, fantasia).
+anime(the_7_deadly_sins, fantasia).
+anime(magi, fantasia).
 
-% Notas dos animes de fantasia
+% Notas dos animes de Fantasia
 nota(jojos_bizarre_adventure, 8.6).
 nota(demon_slayer, 8.8).
 nota(fairy_tail, 8.1).
 nota(bleach, 8.3).
 nota(fullmetal_alchemist, 8.7).
+nota(fate_zero, 8.8).
+nota(attack_on_titan, 8.9).
+nota(naruto, 8.5).
+nota(the_7_deadly_sins, 8.0).
+nota(magi, 8.4).
 
 % Drama
 
-% Filmes de drama
+% Filmes de Drama
 filme(o_resgate_do_shawshank, drama).
 filme(os_12_homens_decentes, drama).
 filme(clube_da_luta, drama).
 filme(forrest_gump, drama).
 filme(a_lista_de_schindler, drama).
 
-% Notas dos filmes de drama
+% Notas dos filmes de Drama
 nota(o_resgate_do_shawshank, 9.3).
 nota(os_12_homens_decentes, 9.0).
 nota(clube_da_luta, 8.8).
 nota(forrest_gump, 8.8).
 nota(a_lista_de_schindler, 9.0).
 
-% Séries de drama
+% Séries de Drama
 serie(breaking_bad, drama).
-serie(o_office, drama).
-serie(amigos, drama).
+serie(the_office, drama).
+serie(friends, drama).
 serie(bojack_horseman, drama).
 serie(the_crown, drama).
+serie(big_little_lies, drama).
+serie(suits, drama).
+serie(nashville, drama).
+serie(the_american, drama).
 
-% Notas das séries de drama
+% Notas das séries de Drama
 nota(breaking_bad, 9.5).
-nota(o_office, 8.8).
-nota(amigos, 8.9).
+nota(the_office, 8.8).
+nota(friends, 8.9).
 nota(bojack_horseman, 8.4).
 nota(the_crown, 8.7).
+nota(big_little_lies, 8.6).
+nota(suits, 8.5).
+nota(nashville, 7.7).
+nota(the_american, 8.4).
 
-% Animes de drama
+% Animes de Drama
 anime(steins_gate, drama).
 anime(death_note, drama).
 anime(attack_on_titan, drama).
 anime(jojos_bizarre_adventure, drama).
 anime(orange, drama).
+anime(anohana, drama).
+anime(clannad, drama).
+anime(violet_evergarden, drama).
+anime(fruits_basket, drama).
+anime(angel_beats, drama).
 
-% Notas dos animes de drama
+% Notas dos animes de Drama
 nota(steins_gate, 8.8).
 nota(death_note, 9.0).
 nota(attack_on_titan, 8.9).
 nota(jojos_bizarre_adventure, 8.6).
 nota(orange, 8.4).
+nota(anohana, 8.7).
+nota(clannad, 9.0).
+nota(violet_evergarden, 8.9).
+nota(fruits_basket, 8.6).
+nota(angel_beats, 8.1).
 
 % Terror
 
-% Filmes de terror
+% Filmes de Terror
 filme(o_exorcista, terror).
 filme(psicose, terror).
 filme(it, terror).
@@ -270,7 +366,7 @@ filme(o_hospede_perfeito, terror).
 filme(babadook, terror).
 filme(o_silencio_dos_inocentes, terror).
 
-% Notas dos filmes de terror
+% Notas dos filmes de Terror
 nota(o_exorcista, 8.8).
 nota(psicose, 8.5).
 nota(it, 7.9).
@@ -282,37 +378,57 @@ nota(o_hospede_perfeito, 7.2).
 nota(babadook, 7.4).
 nota(o_silencio_dos_inocentes, 8.6).
 
-% Séries de terror
+% Séries de Terror
 serie(stranger_things, terror).
 serie(the_walking_dead, terror).
 serie(bates_motel, terror).
 serie(scream_queens, terror).
 serie(the_haunting_of_hill_house, terror).
+serie(hannibal, terror).
+serie(the_terror, terror).
+serie(asylum, terror).
+serie(american_horror_story, terror).
+serie(the_strain, terror).
 
-% Notas das séries de terror
+% Notas das séries de Terror
 nota(stranger_things, 8.8).
 nota(the_walking_dead, 8.3).
 nota(bates_motel, 8.0).
 nota(scream_queens, 7.5).
 nota(the_haunting_of_hill_house, 8.6).
+nota(hannibal, 8.5).
+nota(the_terror, 8.0).
+nota(asylum, 7.6).
+nota(american_horror_story, 8.3).
+nota(the_strain, 7.7).
 
-% Animes de terror
+% Animes de Terror
 anime(tokyo_ghoul, terror).
 anime(another, terror).
 anime(darling_in_the_franxx, terror).
 anime(seraph_of_the_end, terror).
 anime(parasyte, terror).
+anime(psycho_pass, terror).
+anime(hellsing, terror).
+anime(elfen_lied, terror).
+anime(blood_c, terror).
+anime(shiki, terror).
 
-% Notas dos animes de terror
+% Notas dos animes de Terror
 nota(tokyo_ghoul, 8.0).
 nota(another, 8.2).
 nota(darling_in_the_franxx, 7.5).
 nota(seraph_of_the_end, 7.9).
 nota(parasyte, 8.1).
+nota(psycho_pass, 8.6).
+nota(hellsing, 8.4).
+nota(elfen_lied, 8.3).
+nota(blood_c, 7.6).
+nota(shiki, 8.0).
 
 % Comédia
 
-% Filmes de comédia
+% Filmes de Comédia
 filme(o_grande_lebowski, comedia).
 filme(mr_bean, comedia).
 filme(a_vida_e_bela, comedia).
@@ -324,7 +440,7 @@ filme(um_conta_acontecimento_em_outro_conta, comedia).
 filme(alguem_ali, comedia).
 filme(o_grande_grande_amor, comedia).
 
-% Notas dos filmes de comédia
+% Notas dos filmes de Comédia
 nota(o_grande_lebowski, 8.4).
 nota(mr_bean, 8.2).
 nota(a_vida_e_bela, 8.0).
@@ -336,30 +452,50 @@ nota(um_conta_acontecimento_em_outro_conta, 8.3).
 nota(alguem_ali, 8.2).
 nota(o_grande_grande_amor, 8.6).
 
-% Séries de comédia
-serie(amigos, comedia).
+% Séries de Comédia
+serie(friends, comedia).
 serie(breaking_bad, comedia).
 serie(comedians_in_cars_getting_coffee, comedia).
 serie(schitts_creek, comedia).
-serie(o_office_us, comedia).
+serie(the_office, comedia).
+serie(the_good_place, comedia).
+serie(parks_and_rec, comedia).
+serie(community, comedia).
+serie(mother, comedia).
+serie(big_bang_theory, comedia).
 
-% Notas das séries de comédia
-nota(amigos, 8.9).
+% Notas das séries de Comédia
+nota(friends, 8.9).
 nota(breaking_bad, 9.5).
 nota(comedians_in_cars_getting_coffee, 8.5).
 nota(schitts_creek, 8.7).
-nota(o_office_us, 8.8).
+nota(the_office, 8.8).
+nota(the_good_place, 8.6).
+nota(parks_and_rec, 8.8).
+nota(community, 8.5).
+nota(mother, 8.3).
+nota(big_bang_theory, 8.4).
 
-% Animes de comédia
+% Animes de Comédia
 anime(naruto, comedia).
 anime(lucky_star, comedia).
 anime(sword_art_online, comedia).
 anime(dragons, comedia).
 anime(space_dandy, comedia).
+anime(one_punch_man, comedia).
+anime(kono_subarashii_sekai_ni_shukufuku_wo, comedia).
+anime(no_game_no_life, comedia).
+anime(ouran_high_school_host_club, comedia).
+anime(itsudatte_bokura_no_koi_wa_10_cm_datte, comedia).
 
-% Notas dos animes de comédia
+% Notas dos animes de Comédia
 nota(naruto, 8.5).
 nota(lucky_star, 7.8).
 nota(sword_art_online, 8.1).
 nota(dragons, 7.2).
 nota(space_dandy, 8.3).
+nota(one_punch_man, 8.7).
+nota(kono_subarashii_sekai_ni_shukufuku_wo, 8.4).
+nota(no_game_no_life, 8.6).
+nota(ouran_high_school_host_club, 8.2).
+nota(itsudatte_bokura_no_koi_wa_10_cm_datte, 7.9).
